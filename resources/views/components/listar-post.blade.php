9700caf2a042c0187f8e85faa54170a9c67e8bdf
hugoybrahim <ybra72@gmail.com>
2022-08-14 19:40:08 -0400

    Deployment Start

---
+++ resources/views/components/listar-post.blade.php
@@ -0,0 +1,30 @@
+<div>
+{{-- Esta es otra forma de mostrar los posts --}}
+    {{-- @forelse ($posts as $post)
+        <h1>{{ $post->titulo }}</h1>
+    @empty
+        <p>No hay Posts</p>
+    @endforelse --}} 
+    
+    @if ($posts->count())
+        <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+            @foreach ($posts as $post)
+                <div>
+
+                    <a href="{{ route('posts.show',['post'=>$post, 'user' => $post->user]) }}">
+                        <img src="{{ asset('uploads').'/'. $post->imagen }}" alt="Imagen del post {{ $post->titulo }}">
+                    </a>
+
+                </div>
+            @endforeach
+        </div>
+        <div class="my-10">
+            {{ $posts->links() }}
+        </div>
+    @else
+        <p class="text-gray-600 uppercase text-sm text-center font-bold">
+            No hay posts, sigue a alguien para poder mostrar sus posts.
+        </p>
+
+    @endif
+</div>
